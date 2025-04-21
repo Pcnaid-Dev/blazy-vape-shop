e93d36f06d03c841bb6ade86f8f89ea814f04f79
Pcnaid Inc <Support@pcnaid.com>
2025-04-21 16:04:14 -0500

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+// app/layout.tsx
+import React from 'react'
+import type { ReactNode } from 'react'
+
+export const metadata = {
+  title: 'Blazy Vape Shop',
+  description: 'Vape Shop',
+}
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <head />
+      <body>
+        {children}
+      </body>
+    </html>
+  )
+}
